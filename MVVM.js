678fb05f8e28f886f8851602eec06adf2b47e74e
tianshiliang <shiliang.tian@bizgo.com.cn>
2021-03-19 16:29:01 +0800

    初次提交

---
+++ MVVM.js
@@ -0,0 +1,74 @@
+class MVVM{
+    constructor(params) {
+        var app = document.querySelector(params.el);
+        this.dataKeyMapDom = {};
+        // 对数据进行劫持,在set里面处理DOM
+        this.initData(params.data);
+        // 递归遍历DOM数，通过data的key建立key与DOM的映射
+        this.dataKeyMapDomFun(app);
+        // 事件绑定，执行事件后刷新数据
+        this.bindEvent();
+     };
+     initData(data) {
+        const _this = this;
+        this.data = {};
+        for(let key in data){
+            Object.defineProperty(this.data,key,{
+                get(){
+                    console.log('get数据：',key,data[key]);
+                    return data[key];
+                },
+                set(newValue){
+                    console.log('set数据：',key,newValue);
+                    data[key] = newValue;
+                    // 更改视图
+                    _this.dataKeyMapDom[key] && _this.dataKeyMapDom[key].forEach(element=>{
+                        element.innerHTML = newValue;
+                    })
+                }
+            })
+        }
+     }
+     dataKeyMapDomFun (el=app) {
+        if (el.childNodes.length) {
+            el.childNodes.forEach(element => {
+                let value = element.nodeValue;
+                // nodeType:3为文本域，nodeType:1为html标签
+                if (value) {
+                    value= value.trim();
+                    // 文本
+                    if (element.nodeType==3 && /\{\{(.+?)\}\}/.test(value)) {
+                        const key = value.match(/\{\{(.+?)\}\}/)[1];
+                        if (this.dataKeyMapDom[key]) {
+                            this.dataKeyMapDom[key].push(element.parentNode);
+                        } else {
+                            this.dataKeyMapDom[key] = [element.parentNode];
+                        }
+                        element.parentNode.innerHTML = this.data[key]||'';
+                    }
+                } else {
+                    // html标签
+                    if (element.nodeType==1) {
+                        this.dataKeyMapDomFun(element);
+                    }
+                }
+            });
+        }
+        
+   }
+   bindEvent() {
+        // 绑定事件处理函数并且修改
+        const _this = this;
+        const allInputs = document.querySelectorAll('input');
+        allInputs.forEach((input)=>{
+            const key = input.getAttribute('v-model');
+            console.log(key);
+            if (key) {
+                input.onkeyup = function() {
+                    _this.data[key] = input.value;
+                }
+            }
+        })
+   }
+   
+}
